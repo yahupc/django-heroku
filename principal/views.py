4460c51e54c30e10ebc175a19612219265797274
Pablo <yahu39@users.noreply.github.com>
2013-09-22 22:32:11 -0500

    Update views.py

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
 def index_view(request):
-	html = "Probando DJANGO en Heroku!!!"
+	html = "<body style = "background: blue">
+	<h1>Probando DJANGO en Heroku!!!</h1>
+	</body>"
 	return HttpResponse(html)
