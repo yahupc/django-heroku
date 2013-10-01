1c5c8654ad77c67c30f6e2d9f80975fc88fc967e
Pablo Yafac <yahu39pc@gmail>
2013-10-01 00:45:20 -0500

    base de datos-bebidas1

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-#from principal.models import post
+from principal.models import Bebida
+from django.shortcuts import render_to_response
 
 def index_view(request):
 
@@ -10,3 +11,7 @@ def post(request, id):
 	id1 = int(id)
 	html = id1
 	return HttpResponse(html)
+
+def lista_bebidas(request):
+	bebidas = Bebida.objects.all()
+	return render_to_response('lista_bebidas.html',{'lista':bebidas})
