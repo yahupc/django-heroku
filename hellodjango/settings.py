1f65c754ddf1e54958d5e59bfabdf990798d84f0
Pablo Yafac <yahu39pc@gmail>
2013-10-06 12:30:07 -0500

    prueba

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -177,9 +177,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'plantillas'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'plantillas'),
 )
