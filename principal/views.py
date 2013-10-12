814675937944bcd5cacbb6acdb5e366fcccdf7c0
Pablo Yafac <yahu39pc@gmail>
2013-10-12 15:15:21 -0500

    pagina de recetas

--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,7 @@
 from principal.models import Receta, Comentario
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.template import RequestContext
 from principal.models import Bebida
 from django.shortcuts import render_to_response, get_object_or_404
 
