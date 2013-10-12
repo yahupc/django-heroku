339d84476de5ed4d488e38cf812cd71995ae57ac
Pablo Yafac <yahu39pc@gmail>
2013-10-12 16:37:32 -0500

    pagina de recetas

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -58,7 +58,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH,"carga")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://http://pure-refuge-8527.herokuapp.com/media/'
+MEDIA_URL = 'http://pure-refuge-8527.herokuapp.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
