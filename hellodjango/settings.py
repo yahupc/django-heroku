9ef3111c066302c127c81282f0a70e73e33f99c3
Pablo Yafac <yahu39pc@gmail>
2013-10-16 01:57:19 -0500

    plantilla base en paginas

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -58,13 +58,13 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH,"carga")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://pure-refuge-8527.herokuapp.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -177,9 +177,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'plantillas'
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'plantillas'),
+    os.path.join(BASE_DIR, 'static'),
 )
