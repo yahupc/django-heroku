49eb542a3a8f2d16ed06a15323d1d5a4f903142d
Pablo Yafac <yahu39pc@gmail>
2013-10-19 19:31:11 -0500

    formulario contacto

--- principal/forms.py
+++ principal/forms.py
@@ -1,8 +1,8 @@
-	#encoding:utf-8
-	from django.forms import ModelForm
-	from django import forms
-	from principal.models import Receta, Comentario
+#encoding:utf-8
+from django.forms import ModelForm
+from django import forms
+from principal.models import Receta, Comentario
 
-	class ContactoForm(forms.Form):
-		correo = forms.EmailField(label='Tu correo electrónico')
-		mensaje = forms.CharField(widget=forms.Textarea)
+class ContactoForm(forms.Form):
+	correo = forms.EmailField(label='Tu correo electrónico')
+	mensaje = forms.CharField(widget=forms.Textarea)
