5e70d2dfe25ff36d5d37c56779c8a5e60d4abb09
Pablo Yafac <yahu39pc@gmail>
2013-10-21 00:55:05 -0500

    nousuario, usuario inactivo, ingresar

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -36,7 +36,7 @@ TIME_ZONE = 'America/Lima'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-PE'
+LANGUAGE_CODE = 'es-PE'
 
 SITE_ID = 1
 
